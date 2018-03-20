1a82516eb7f966f44e0c7a03d867b79116e1f6aa
shinrai2 <fuck@shinrai.cc>
2018-03-20 23:57:18 +0800

    2018-3-20 23:57:13

--- src/head/pngdata.go
+++ src/head/pngdata.go
@@ -3,6 +3,13 @@ package head
 // PngFileSignature is the first eight bytes of a PNG file.
 type PngFileSignature [8]byte
 
+type Chunk struct {
+	Length        uint32
+	ChunkTypeCode uint32
+	ChunkData     interface{}
+	CRC           [4]byte
+}
+
 type IHDR struct {
 	Width       uint32
 	Height      uint32
@@ -13,4 +20,11 @@ type IHDR struct {
 	Interlace   byte
 }
 
+type PLTE struct {
+	Red   byte
+	Green byte
+	Blue  byte
+}
+
+// IEND is the last twelve bytes of a PNG file.
 type IEND [12]byte
--- src/png/readpng.go
+++ src/png/readpng.go
@@ -1,1 +1,15 @@
 package png
+
+import (
+	"fmt"
+	"os"
+
+	util "../util"
+)
+
+func Read(filePath *string) {
+	fmt.Println("Input path: ", *filePath)
+	f, err := os.Open(*filePath)
+	util.Check(err)
+	/* Load time. */
+}
