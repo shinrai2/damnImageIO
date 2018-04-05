b8afbf7e0a892cf704fbab3f193bd0bbb487c8d5
shinrai2 <fuck@shinrai.cc>
2018-04-05 16:28:06 +0800

    Automatic selection method according to file suffix

--- src/main.go
+++ src/main.go
@@ -4,11 +4,19 @@ import (
 	"flag"
 
 	bmp "./bmp"
+	png "./png"
 )
 
 // main func.
 func main() {
 	filePath := flag.String("path", "source/t_1.bmp", "the path of file.")
 	flag.Parse()
-	bmp.Read(filePath)
+	// Automatic selection method according to file suffix
+	switch (*filePath)[len(*filePath)-3:] {
+	case "bmp":
+		bmp.Read(filePath)
+	case "jpg":
+	case "png":
+		png.Read(filePath)
+	}
 }
